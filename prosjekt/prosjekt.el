bab6c87f7a5d157a343476810c8d408431a98b0e
Austin Bingham <austin.bingham@gmail.com>
2012-02-25 10:14:04 +0100

    not putting code in different files now.
    Need to rethink that.

--- prosjekt/prosjekt.el
+++ prosjekt/prosjekt.el
@@ -7,51 +7,15 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(load-file "keybinding.el")
-
-;; TODO: Are there better, standard versions of these functions?
-(defun prsj-get-string-from-file (filename)
-  "Return FILENAME's complete file content."
-  (with-temp-buffer
-    (insert-file-contents filename)
-    (buffer-string)))
-
-(defun prsj-write-string-to-file (string filename)
-  "Write STRING as the contents of FILENAME."
-   (with-temp-buffer
-     (insert string)
-     (when (file-writable-p filename)
-       (write-region (point-min)
-                     (point-max)
-                     file))))
-
-; minor-mode-alist entry
+; minor-mode-map-alist entry
 (defvar prosjekt-mode t)
-
-(defvar prsj-config nil 
-  "The global prosjekt configuration.")
 
 (defvar prsj-cur-proj nil
   "The current project definition, nil if no project.")
 
-(defun prsj-config-file ()
-  "Get the global configuration filename (~/.emacs.d/prosjekt.lst)"
-  (expand-file-name 
-   "prosjekt.lst"
-   (if (boundp 'user-emacs-directory) 
-       user-emacs-directory
-     "~/.emacs.d/"
-     )))
-
 (defun prosjekt-startup ()
   "Initialize the global configuration information."
-  (setq prsj-config 
-	(read 
-	 (prsj-get-string-from-file (prsj-config-file)))))
-
-(defun prsj-get-config-item (name)
-  "Get a value from the global config."
-  (cdr (assoc name prsj-config)))
+  (prsj-load-config))
 
 (defun prosjekt-open (proj)
   "Open a project named PROJ."
@@ -70,6 +34,72 @@
     ; TODO: set up command key mappings
     ; TODO: open curfile if it's set
     ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; global config-related functionality
+
+(defvar prsj-config nil 
+  "The global prosjekt configuration.")
+
+(defun prsj-config-file ()
+  "Get the global configuration filename (~/.emacs.d/prosjekt.lst)"
+  (expand-file-name 
+   "prosjekt.lst"
+   (if (boundp 'user-emacs-directory) 
+       user-emacs-directory
+     "~/.emacs.d/"
+     )))
+
+(defun prsj-get-config-item (name)
+  "Get a value from the global config."
+  (cdr (assoc name prsj-config)))
+
+(defun prsj-load-config ()
+  (setq prsj-config 
+	(read 
+	 (prsj-get-string-from-file (prsj-config-file)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; support for reading elisp code from files
+
+;; TODO: Are there better, standard versions of these functions?
+(defun prsj-get-string-from-file (filename)
+  "Return FILENAME's complete file content."
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (buffer-string)))
+
+(defun prsj-write-string-to-file (string filename)
+  "Write STRING as the contents of FILENAME."
+   (with-temp-buffer
+     (insert string)
+     (when (file-writable-p filename)
+       (write-region (point-min)
+                     (point-max)
+                     file))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Routines for dealing with the keymap and bindings.
+
+(defun prsj-get-mode-map ()
+  "Get the (mode . keymap) cell from minor-mode-map-alist.
+This will initialize the entry if needed."
+  (let ((m (assoc 'prosjekt-mode minor-mode-map-alist)))
+    (or	m
+	(let ((mode (cons 'prosjekt-mode (make-sparse-keymap))))
+	  (car (push mode minor-mode-map-alist))))))
+
+(defun prsj-reset-keys ()
+  "Clear the keybindings for the minor mode."
+  (setcdr (prsj-get-mode-map) (make-sparse-keymap)))
+		   
+(defun prsj-setkeys (bindings)
+  "Set a series of bindings in the minor mode.
+``bindings`` is an alist if (keycode.function)."
+  (let ((keymap (cdr (prsj-get-mode-map))))
+    (dolist (b bindings)
+      (define-key keymap (read (car b)) (cdr b))
+    )))
 
 ;;   ; If a is just a project name, look up the (name.dir) cell in the
 ;;   ; project list.
