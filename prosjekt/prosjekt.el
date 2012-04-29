99f91ac16ce559b55cf5b374698645ebfd7a1773
Austin Bingham <austin.bingham@gmail.com>
2012-04-29 17:45:21 +0200

    Added a lot of documentation to the main .el.

--- prosjekt/prosjekt.el
+++ prosjekt/prosjekt.el
@@ -1,12 +1,72 @@
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; prosjekt.el --- project workspaces for emacs
+;; prosjekt.el --- a software project tool for emacs
 ;;
-;; Copyright (C) 2012 Austin Bingham
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/abingham/prosjekt
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This file is not part of GNU Emacs.
+;;
+;; Copyright (c) 2012 Austin Bingham
+;;
+;; Description:
+;;
+;; prosjekt is a simple software project management tool. A project
+;; in prosjekt comprises 1) a top-level directory, 2) a collection
+;; of files belonging to the project, and 3) a set of commands that
+;; can be executed.
+;;
+;; For more details, see the project page at
+;; https://github.com/abingham/prosjekt.
+;;
+;; Installation:
+;;
+;; Copy prosjekt.el to some location in your emacs load path. Then add
+;; "(require 'prosjekt)" to your emacs initialization (.emacs,
+;; init.el, or something). 
+;; 
+;; Installation (anything integration):
+;; 
+;; Prosjekt comes with integration with anything
+;; (http://emacswiki.org/emacs/Anything). To enable this, copy
+;; anything-prosjekt.el to your emacs load path. Then add "(require
+;; 'anything-prosjekt)" to you emacs initialization. This provides the
+;; anything sources "anything-c-source-prosjekt-files" and
+;; "anything-c-source-prosjekt-projects".
+;;
+;; Example config:
+;; 
+;;   (require 'prosjekt)
+;;   (require 'anything-prosjekt)
+;; 
+;;   (require 'anything)
+;;   (add-to-list 'anything-sources 'anything-c-source-prosjekt-files t)
+;;   (add-to-list 'anything-sources 'anything-c-source-prosjekt-projects t)
+;; 
+;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; public API
 
 ; minor-mode-map-alist entry
