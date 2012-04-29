9cde4c55497ca86baa6ee16730de801779bc798c
Austin Bingham <austin.bingham@gmail.com>
2012-04-29 17:47:10 +0200

    Added license info to anything-prosjekt

--- prosjekt/anything-prosjekt.el
+++ prosjekt/anything-prosjekt.el
@@ -2,6 +2,8 @@
 
 ;; Copyright (C) 2012 Austin Bingham
 ;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/abingham/prosjekt
 
 ;; Originally based on anything-eproject by Daniel Hackney
 ;; http://www.emacswiki.org/emacs/anything-eproject.el
@@ -29,8 +31,29 @@
 ;;   (add-to-list 'anything-sources 'anything-c-source-prosjekt-files t)
 ;;   (add-to-list 'anything-sources 'anything-c-source-prosjekt-projects t)
 ;;
-;; Prosjekt: https://github.com/abingham/prosjekt
 ;; Anything: http://www.emacswiki.org/emacs/Anything
+;;
+;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Code:
 
